e4c74841b25fe1f6aef48964d221762ce3dcbdbf
OwenLaRosa <owen.larosa@outlook.com>
2016-10-30 19:09:38 -0400

    fix: Crashing with OkHttp and endpoints
    Use wrapper for vanilla HttpUrlConnection instead

--- login_server/src/main/java/com/example/owen/myapplication/backend/MyEndpoint.java
+++ login_server/src/main/java/com/example/owen/myapplication/backend/MyEndpoint.java
@@ -14,23 +14,19 @@ import com.google.api.server.spi.config.ApiNamespace;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.inject.Named;
 
-import okhttp3.FormBody;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
-
 /**
  * An endpoint class we are exposing
  */
 @Api(
-        name = "api",
+        name = "myApi",
         version = "v1",
         namespace = @ApiNamespace(
                 ownerDomain = "backend.myapplication.owen.example.com",
@@ -54,30 +50,37 @@ public class MyEndpoint {
     private static final String PARAM_USERNAME = "username";
     private static final String PARAM_PASSWORD = "password";
 
-    private static final String FIREBASE_SECRET = "";
+    private static final String FIREBASE_SECRET = "<FIREBASE_SECRET>";
 
-    private OkHttpClient mClient = new OkHttpClient();
+    //private OkHttpClient mClient = new OkHttpClient();
 
     @ApiMethod(name = "session", path = "session", httpMethod = ApiMethod.HttpMethod.POST)
     public SessionResponse session(@Named("username") String username, @Named("password") String password) {
         SessionResponse result = new SessionResponse();
+        // test values to return even if the login fails
+        // this is to test the 500 error if the rest of the code isn't working
 
-        // attempt to login with user's credentials
-        RequestBody formBody = new FormBody.Builder()
-                .add(PARAM_USERNAME, username)
-                .add(PARAM_PASSWORD, password)
-                .build();
-        Request request = new Request.Builder()
-                .url(UDACITY_SESSION_URL)
-                .post(formBody)
-                .build();
-        String loginResult = "";
         try {
-            // parse the result
-            Response response = mClient.newCall(request).execute();
-            loginResult = response.body().string();
-            JSONObject root = new JSONObject(loginResult);
-            JSONObject account = root.getJSONObject(KEY_ACCOUNT);
+            // attempt to login with user's credentials
+            HttpRequest request = new HttpRequest(UDACITY_SESSION_URL);
+            // make sure the data is read as JSON
+            request.con.addRequestProperty("Accept", "application/json");
+            request.con.addRequestProperty("Content-Type", "application/json");
+            // set the JSON body for post request
+            JSONObject credentials = new JSONObject();
+            JSONObject parent = new JSONObject();
+            credentials.put(PARAM_USERNAME, username);
+            credentials.put(PARAM_PASSWORD, password);
+            parent.put("udacity", credentials);
+            // set the JSON body
+            BufferedWriter jsonBody = new BufferedWriter(new OutputStreamWriter(request.con.getOutputStream()));
+            jsonBody.write(parent.toString());
+            jsonBody.close();
+            String root = request.prepare(HttpRequest.Method.POST).sendAndReadString();
+            // return the json to the client for debugging purposes
+            result.setToken(root);
+            result.setSuccess(true);
+            /*JSONObject account = root.getJSONObject(KEY_ACCOUNT);
             boolean registered = account.getBoolean(KEY_REGISTERED);
             String key = account.getString(KEY_KEY);
             // specify whether or not the login succeeded
@@ -85,11 +88,11 @@ public class MyEndpoint {
             if (registered) {
                 // user logged in successfully, create their token
                 result.setToken(generateTokenForId(key));
-            }
+            }*/
         } catch (IOException e) {
-            result.setSuccess(false);
+
         } catch (JSONException e) {
-            result.setSuccess(false);
+
         }
         return result;
     }
@@ -99,7 +102,7 @@ public class MyEndpoint {
      * @param userId Unique identifier, same ID used by Udacity
      * @return The user's auth token
      */
-    private String generateTokenForId(String userId) {
+    private String generateTokenForId(Integer userId) {
         // based on the example from https://www-staging.firebase.com/docs/android/guide/login/custom.html
         Map<String, Object> payload = new HashMap<String, Object>();
         payload.put("uid", userId);
