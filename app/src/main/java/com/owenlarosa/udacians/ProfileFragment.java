497eca7aeb989095bf389fac23c76a6d1c959eaa
OwenLaRosa <owen.larosa@outlook.com>
2016-11-11 12:19:30 -0500

    feat: Add properties and onclicks for views

--- app/src/main/java/com/owenlarosa/udacians/ProfileFragment.java
+++ app/src/main/java/com/owenlarosa/udacians/ProfileFragment.java
@@ -6,8 +6,17 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.owenlarosa.udacians.views.WritePostView;
+
+import org.w3c.dom.Text;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import butterknife.Unbinder;
 
 /**
@@ -15,6 +24,21 @@ import butterknife.Unbinder;
  */
 
 public class ProfileFragment extends Fragment {
+
+    @BindView(R.id.profile_image_view)
+    ImageView profilePictureImageView;
+    @BindView(R.id.profile_name_text_view)
+    TextView nameTextView;
+    @BindView(R.id.profile_title_text_view)
+    TextView titleTextView;
+    @BindView(R.id.profile_about_text_view)
+    TextView aboutTextView;
+    @BindView(R.id.profile_links_linear_layout)
+    LinearLayout linksLinearLayout;
+    @BindView(R.id.profile_write_post_view)
+    WritePostView writePostView;
+    @BindView(R.id.profile_posts_linear_layout)
+    LinearLayout postsLinearLayout;
 
     private Unbinder mUnbinder;
 
@@ -25,5 +49,10 @@ public class ProfileFragment extends Fragment {
         mUnbinder = ButterKnife.bind(this, rootView);
 
         return rootView;
+    }
+
+    @OnClick(R.id.connect_button)
+    public void connectTapped(View view) {
+
     }
 }
