fddca9a190ab2df0aa3094982bbc7542bdf73a8b
OwenLaRosa <owen.larosa@outlook.com>
2016-11-13 11:46:48 -0500

    feat: Add views for chat selection list

---
+++ app/src/main/java/com/owenlarosa/udacians/ChatsListFragment.java
@@ -0,0 +1,35 @@
+package com.owenlarosa.udacians;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+/**
+ * Created by Owen LaRosa on 11/12/16.
+ */
+
+public class ChatsListFragment extends Fragment {
+
+    @BindView(R.id.chats_recycler_view)
+    RecyclerView recyclerView;
+
+    Unbinder mUnbinder;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_chats_list, container, false);
+        mUnbinder = ButterKnife.bind(this, rootView);
+
+        return rootView;
+    }
+
+}
