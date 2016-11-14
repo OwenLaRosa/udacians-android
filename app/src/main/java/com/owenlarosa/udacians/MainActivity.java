049f6b7f79775c4ea970c30061d4ec7b75966191
OwenLaRosa <owen.larosa@outlook.com>
2016-11-14 13:13:58 -0500

    feat: Populate nav drawer with item titles

--- app/src/main/java/com/owenlarosa/udacians/MainActivity.java
+++ app/src/main/java/com/owenlarosa/udacians/MainActivity.java
@@ -1,13 +1,28 @@
 package com.owenlarosa.udacians;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.left_drawer)
+    ListView navigationDrawer;
+
+    Unbinder unbinder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        unbinder = ButterKnife.bind(this);
+
+        // populate the nav drawer with its static content
+        navigationDrawer.setAdapter(new NavigationDrawerAdapter(this));
     }
 }
