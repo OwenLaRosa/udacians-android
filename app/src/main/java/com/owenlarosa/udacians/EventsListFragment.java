edbfec6efb171b4f5cbecc491571edb280b5a4ef
OwenLaRosa <owen.larosa@outlook.com>
2016-11-14 15:52:08 -0500

    feat: Add fragment for events list

---
+++ app/src/main/java/com/owenlarosa/udacians/EventsListFragment.java
@@ -0,0 +1,34 @@
+package com.owenlarosa.udacians;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+/**
+ * Created by Owen LaRosa on 11/14/16.
+ */
+
+public class EventsListFragment extends Fragment {
+
+    @BindView(R.id.events_recycler_view)
+    RecyclerView recyclerView;
+
+    private Unbinder mUnbinder;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_events_list, container, false);
+        mUnbinder = ButterKnife.bind(this, rootView);
+
+        return rootView;
+    }
+}
