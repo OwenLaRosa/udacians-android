921f303acc4aa1e9309eae23ec8303e6431d9fe1
OwenLaRosa <owen.larosa@outlook.com>
2016-11-14 16:21:47 -0500

    feat: Add fragments for settings and help

---
+++ app/src/main/java/com/owenlarosa/udacians/SettingsFragment.java
@@ -0,0 +1,30 @@
+package com.owenlarosa.udacians;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+/**
+ * Created by Owen LaRosa on 11/14/16.
+ */
+
+public class SettingsFragment extends Fragment {
+
+    private Unbinder mUnbinder;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
+        mUnbinder = ButterKnife.bind(this, rootView);
+
+        return rootView;
+    }
+
+}
