82dd1456405569fa416459e21e5fccd5a9ede136
OwenLaRosa <owen.larosa@outlook.com>
2016-11-14 14:55:16 -0500

    feat: Respond to clicks on nav drawer list items

--- app/src/main/java/com/owenlarosa/udacians/MainActivity.java
+++ app/src/main/java/com/owenlarosa/udacians/MainActivity.java
@@ -1,7 +1,12 @@
 package com.owenlarosa.udacians;
 
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.FrameLayout;
 import android.widget.ListView;
 
 import butterknife.BindView;
@@ -10,6 +15,10 @@ import butterknife.Unbinder;
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+    @BindView(R.id.view_container)
+    FrameLayout viewContainer;
     @BindView(R.id.left_drawer)
     ListView navigationDrawer;
 
@@ -24,5 +33,28 @@ public class MainActivity extends AppCompatActivity {
 
         // populate the nav drawer with its static content
         navigationDrawer.setAdapter(new NavigationDrawerAdapter(this));
+        navigationDrawer.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            private View lastSelectedView;
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if (i == 0) {
+                    // show user's profile info
+                } else {
+                    // reset color of previous view, highlight selected view's background
+                    if (lastSelectedView != null) {
+                        lastSelectedView.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                    }
+                    view.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+                    lastSelectedView = view;
+
+                    // TODO: insert appropriate fragment into frame layout
+
+                    // after updaing the layout, dismiss the drawer
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                }
+            }
+        });
     }
 }
