b4917df72d4aee55133260f0b0588be459d8b3e1
OwenLaRosa <owen.larosa@outlook.com>
2016-11-16 14:50:08 -0500

    feat: Add properties and methods, fix label text

--- app/src/main/java/com/owenlarosa/udacians/EditProfileFragment.java
+++ app/src/main/java/com/owenlarosa/udacians/EditProfileFragment.java
@@ -6,8 +6,12 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import butterknife.Unbinder;
 
 /**
@@ -15,6 +19,25 @@ import butterknife.Unbinder;
  */
 
 public class EditProfileFragment extends Fragment {
+
+    @BindView(R.id.edit_profile_image_button)
+    ImageButton profileImageButton;
+    @BindView(R.id.edit_first_name_text_field)
+    EditText firstNameEditText;
+    @BindView(R.id.edit_last_name_text_field)
+    EditText lastNameEditText;
+    @BindView(R.id.edit_title_text_field)
+    EditText titleEditText;
+    @BindView(R.id.edit_about_text_field)
+    EditText aboutEditText;
+    @BindView(R.id.edit_site_text_field)
+    EditText siteEditText;
+    @BindView(R.id.edit_blog_text_field)
+    EditText blogEditText;
+    @BindView(R.id.edit_linkedin_text_field)
+    EditText linkedinEditText;
+    @BindView(R.id.edit_twitter_text_field)
+    EditText twitterEditText;
 
     Unbinder mUnbinder;
 
@@ -25,6 +48,16 @@ public class EditProfileFragment extends Fragment {
         mUnbinder = ButterKnife.bind(this, rootView);
 
         return rootView;
+    }
+
+    @OnClick(R.id.edit_profile_image_button)
+    public void chooseImage() {
+
+    }
+
+    @OnClick(R.id.edit_reset_button)
+    public void resetChanges() {
+
     }
 
 }
