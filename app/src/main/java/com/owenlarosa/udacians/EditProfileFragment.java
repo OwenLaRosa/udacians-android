c7490fc1c07fc93334c100753f4df0c963aef4d1
OwenLaRosa <owen.larosa@outlook.com>
2016-12-08 11:34:05 -0500

    feat: Load profile picture into button

--- app/src/main/java/com/owenlarosa/udacians/EditProfileFragment.java
+++ app/src/main/java/com/owenlarosa/udacians/EditProfileFragment.java
@@ -1,16 +1,26 @@
 package com.owenlarosa.udacians;
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
+import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -24,11 +34,17 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
 
+import static android.app.Activity.RESULT_OK;
+import static android.view.View.VISIBLE;
+
 /**
  * Created by Owen LaRosa on 11/16/16.
  */
 
 public class EditProfileFragment extends Fragment {
+
+    // user selected an image from the gallery
+    private static final int RESULT_PICK_IMAGE = 1;
 
     @BindView(R.id.edit_profile_image_button)
     ImageButton profileImageButton;
@@ -46,16 +62,22 @@ public class EditProfileFragment extends Fragment {
     EditText twitterEditText;
 
     Unbinder mUnbinder;
+    private Context mContext;
 
     FirebaseDatabase mFirebaseDatabase;
     DatabaseReference mBasicReference;
     DatabaseReference mProfileReference;
+
+    // image currently displayed in post authoring view
+    private Bitmap mImage;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_edit_profile, container, false);
         mUnbinder = ButterKnife.bind(this, rootView);
+
+        mContext = getActivity();
 
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         DatabaseReference userReference = mFirebaseDatabase.getReference().child("users").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
@@ -80,9 +102,32 @@ public class EditProfileFragment extends Fragment {
         }
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == RESULT_PICK_IMAGE && resultCode == RESULT_OK && null != intent) {
+            Uri imagePath = intent.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getActivity().getContentResolver().query(imagePath,
+                    filePathColumn, null, null, null);
+            if (null != cursor && cursor.moveToFirst()) {
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                String picturePath = cursor.getString(columnIndex);
+                cursor.close();
+                // show selected bitmap in the preview pane
+                mImage = BitmapFactory.decodeFile(picturePath);
+                profileImageButton.setVisibility(VISIBLE);
+                profileImageButton.setImageBitmap(mImage);
+            }
+        }
+    }
+
     @OnClick(R.id.edit_profile_image_button)
     public void chooseImage() {
-
+        // make sure read permissions are enabled before loading image
+        verifyStoragePermissions((Activity) mContext);
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, RESULT_PICK_IMAGE);
     }
 
     @OnClick(R.id.edit_reset_button)
@@ -102,9 +147,15 @@ public class EditProfileFragment extends Fragment {
                     getActivity().setTitle(basicProfile.getName());
                     titleEditText.setText(basicProfile.getTitle() != null ? basicProfile.getTitle() : "");
                     aboutEditText.setText(basicProfile.getAbout() != null ? basicProfile.getAbout() : "");
+                    if (basicProfile.getPhoto() != null) {
+                        Glide.with(mContext)
+                                .load(basicProfile.getPhoto())
+                                .into(profileImageButton);
+                    }
                 }
                 titleEditText.setEnabled(true);
                 aboutEditText.setEnabled(true);
+                profileImageButton.setEnabled(true);
             }
 
             @Override
@@ -145,6 +196,36 @@ public class EditProfileFragment extends Fragment {
         mProfileReference.child("blog").setValue(blogEditText.getText().toString());
         mProfileReference.child("linkedin").setValue(linkedinEditText.getText().toString());
         mProfileReference.child("twitter").setValue(twitterEditText.getText().toString());
+    }
+
+    // on Android Marshmallow and later, permissions for reading the image must be requested at runtime
+    // see: http://stackoverflow.com/questions/8854359/exception-open-failed-eacces-permission-denied-on-android
+
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            android.Manifest.permission.READ_EXTERNAL_STORAGE
+    };
+
+    /**
+     * Checks if the app has permission to write to device storage
+     *
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
     }
 
 }
