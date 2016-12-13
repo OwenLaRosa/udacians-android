1b23b56345cea7bd12d5a363b8a3da3942a25583
OwenLaRosa <owen.larosa@outlook.com>
2016-12-13 18:56:25 -0500

    feat: Implement bug reporting

--- app/src/main/java/com/owenlarosa/udacians/HelpFragment.java
+++ app/src/main/java/com/owenlarosa/udacians/HelpFragment.java
@@ -6,8 +6,18 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
+
+import java.util.HashMap;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import butterknife.Unbinder;
 
 /**
@@ -16,15 +26,35 @@ import butterknife.Unbinder;
 
 public class HelpFragment extends Fragment {
 
+    @BindView(R.id.report_bug_edit_text)
+    EditText reportBugEditText;
+
     private Unbinder mUnbinder;
+
+    FirebaseDatabase mFirebaseDatabase;
+    DatabaseReference bugsReference;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_help, container, false);
         mUnbinder = ButterKnife.bind(this, rootView);
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        bugsReference = mFirebaseDatabase.getReference().child("bugs");
 
         return rootView;
+    }
+
+    @OnClick(R.id.report_bug_button)
+    public void reportBug() {
+        String report = reportBugEditText.getText().toString();
+        String user = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("user", user);
+        data.put("report", report);
+        data.put("time", ServerValue.TIMESTAMP);
+        bugsReference.push().setValue(data);
+        reportBugEditText.setText("");
     }
 
 }
