086825c888bc73584b23fddeaf1f5073cd6d2074
OwenLaRosa <owen.larosa@outlook.com>
2016-12-17 11:01:15 -0500

    feat: Add content provider for jobs

---
+++ app/src/main/java/com/owenlarosa/udacians/contentprovider/JobsProvider.java
@@ -0,0 +1,24 @@
+package com.owenlarosa.udacians.contentprovider;
+
+import android.net.Uri;
+
+import net.simonvt.schematic.annotation.ContentProvider;
+import net.simonvt.schematic.annotation.ContentUri;
+import net.simonvt.schematic.annotation.TableEndpoint;
+
+/**
+ * Created by Owen LaRosa on 12/17/16.
+ */
+
+@ContentProvider(authority = JobsProvider.AUTHORITY, database = JobsDatabase.class)
+public class JobsProvider {
+
+    public static final String AUTHORITY = "com.owenlarosa.udacians.contentprovider.JobsProvider";
+
+    @TableEndpoint(table = JobsDatabase.JOBS) public static class Jobs {
+        @ContentUri(path = "jobs",
+        type = "vnd.android.cursor.dir/job",
+        defaultSort = JobsListColumns.DATE + " ASC")
+        public static final Uri JOBS = Uri.parse("content://" + AUTHORITY + "/jobs");
+    }
+}
