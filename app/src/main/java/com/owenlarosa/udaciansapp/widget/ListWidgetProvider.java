dbfab75c4521a45eee87950fa3e62698984ef4e8
OwenLaRosa <owen.larosa@outlook.com>
2016-12-23 15:24:24 -0500

    feat: Add collection widget for jobs

---
+++ app/src/main/java/com/owenlarosa/udaciansapp/widget/ListWidgetProvider.java
@@ -0,0 +1,43 @@
+package com.owenlarosa.udaciansapp.widget;
+
+import android.annotation.TargetApi;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.widget.RemoteViews;
+
+import com.owenlarosa.udaciansapp.R;
+
+/**
+ * Created by Owen LaRosa on 12/23/16.
+ */
+
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class ListWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    private void setRemoteAdapter(Context context, @NonNull final RemoteViews views) {
+        views.setRemoteAdapter(R.id.widget_list,
+                new Intent(context, ListWidgetRemoteViewsService.class));
+    }
+
+    @SuppressWarnings("deprecation")
+    private void setRemoteAdapterV11(Context context, @NonNull final RemoteViews views) {
+        views.setRemoteAdapter(0, R.id.widget_list,
+                new Intent(context, ListWidgetRemoteViewsService.class));
+    }
+
+}
---
+++ app/src/main/java/com/owenlarosa/udaciansapp/widget/ListWidgetRemoteViewsService.java
@@ -0,0 +1,100 @@
+package com.owenlarosa.udaciansapp.widget;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Binder;
+import android.widget.AdapterView;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import com.owenlarosa.udaciansapp.R;
+import com.owenlarosa.udaciansapp.contentprovider.JobsListColumns;
+import com.owenlarosa.udaciansapp.contentprovider.JobsProvider;
+
+/**
+ * Created by Owen LaRosa on 12/23/16.
+ */
+
+public class ListWidgetRemoteViewsService extends RemoteViewsService {
+
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new RemoteViewsFactory() {
+
+            private Cursor data = null;
+
+            @Override
+            public void onCreate() {
+
+            }
+
+            @Override
+            public void onDataSetChanged() {
+                if (data != null) {
+                    data.close();
+                }
+
+                // clear and restore data when using non-exported content provider
+                final long identityToken = Binder.clearCallingIdentity();
+                data = getContentResolver().query(JobsProvider.Jobs.JOBS,
+                        null,
+                        null,
+                        null,
+                        null);
+                Binder.restoreCallingIdentity(identityToken);
+            }
+
+            @Override
+            public void onDestroy() {
+                if (data != null) {
+                    data.close();
+                    data = null;
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return data == null ? 0 : data.getCount();
+            }
+
+            @Override
+            public RemoteViews getViewAt(int position) {
+                if (position == AdapterView.INVALID_POSITION || data == null || !data.moveToPosition(position)) {
+                    return null;
+                }
+                RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.jobs_list_item);
+                String title = data.getString(data.getColumnIndex(JobsListColumns.TITLE));
+                remoteViews.setTextViewText(R.id.job_title_text_view, title);
+                String company = data.getString(data.getColumnIndex(JobsListColumns.COMPANY));
+                remoteViews.setTextViewText(R.id.job_company_text_view, company);
+                String location = data.getString(data.getColumnIndex(JobsListColumns.LOCATION));
+                remoteViews.setTextViewText(R.id.job_location_text_view, location);
+
+                return remoteViews;
+            }
+
+            @Override
+            public RemoteViews getLoadingView() {
+                return new RemoteViews(getPackageName(), R.layout.jobs_list_item);
+            }
+
+            @Override
+            public int getViewTypeCount() {
+                return 1;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                if (data.moveToPosition(position)) {
+                    return data.getLong(data.getColumnIndex(JobsListColumns._ID));
+                }
+                return position;
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return true;
+            }
+        };
+    }
+}
